82338fa6e64043c89f63bab906565651711589ea
juliansb <jsbpublico@gmail.com>
2016-03-02 11:30:38 +0100

    Introducida la documentacion.h

---
+++ Documentacion.h
@@ -0,0 +1,26 @@
+/**
+******************************************************
+* @file Documentacion.h
+* @brief Conjunto de funciones para tratamioento de String
+* @author Oscar Salas Mestres & Julian Salas Bartolome
+* @version 1.0
+* @date Agosto 2012
+*
+*
+*******************************************************/
+
+
+
+//-------------------------------------------------------
+//Estructuras de datos para gestion del modulo HLK_RM04 
+//-------------------------------------------------------
+
+/**
+ * \mainpage JSBString.o
+ *
+ * \section introduccion_seccion Introduccion
+ *
+ * Conjuntos de funciones string complementarias a las standard
+ *
+ */
+
